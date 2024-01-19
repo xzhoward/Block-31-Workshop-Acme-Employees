2831bb33ff6de6a7df9f1df8576fceea96ba459b
ericpkatz <ericpkatz@gmail.com>
2024-01-19 09:53:04 -0500

    a commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Guided Practice Readme
+
+Please look at your Canvas Guided Practice for instructions,
+and, if available, your cohort playlist.
---
+++ server/index.js
@@ -0,0 +1,9 @@
+// imports here for express and pg
+
+// static routes here (you only need these for deployment)
+
+// app routes here
+
+// create your init function
+
+// init function invocation
